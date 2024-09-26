4cf4811c7125229816d4d5c058bd824f2015ae94
david <davidyuzhakov17@gmail.com>
2024-09-26 06:57:49 +0300

    add: translate measure

--- src/components/Nft.jsx
+++ src/components/Nft.jsx
@@ -1,21 +1,21 @@
-import { useState } from "react";
-import { Button } from "./Button";
+import { useState } from 'react'
+import { Button } from './Button'
 
-import quote from "../assets/icons/quote-text.svg"
-import arrow from "../assets/icons/arrow-double.svg"
-import damage from "../assets/icons/damage.svg"
-import hp from "../assets/icons/hp.svg"
-import recharge from "../assets/icons/recharge.svg"
+import quote from '../assets/icons/quote-text.svg'
+import arrow from '../assets/icons/arrow-double.svg'
+import damage from '../assets/icons/damage.svg'
+import hp from '../assets/icons/hp.svg'
+import recharge from '../assets/icons/recharge.svg'
 
-import { StatBar } from "./StatBar";
-import { useTranslation } from "react-i18next";
+import { StatBar } from './StatBar'
+import { useTranslation } from 'react-i18next'
 
-export function Nft ({ characters }) {
+export function Nft({ characters }) {
   const { t } = useTranslation()
   const [currentId, setCurrentId] = useState(1)
-  const character = characters.find(item => item.id === currentId)
+  const character = characters.find((item) => item.id === currentId)
 
-  function clickHandler (id) {
+  function clickHandler(id) {
     if (id) {
       return setCurrentId(id)
     }
@@ -27,17 +27,17 @@ export function Nft ({ characters }) {
         <div className="nft-list">
           <img className="nft-list__quote" src={quote} alt="quote" />
           <div className="nft-list__items">
-            {characters.map(item => (
-              <div 
-                key={item.id} 
-                className="nft-list__item" 
-                onClick={() => !item.locked ? clickHandler(item.id) : {}}
+            {characters.map((item) => (
+              <div
+                key={item.id}
+                className="nft-list__item"
+                onClick={() => (!item.locked ? clickHandler(item.id) : {})}
               >
-                <img 
+                <img
                   height={120}
                   width={84}
-                  src={`/akronix/nft/${!item.locked ? item.id : 'coming'}.png`} 
-                  alt="character" 
+                  src={`/akronix/nft/${!item.locked ? item.id : 'coming'}.png`}
+                  alt="character"
                 />
                 <span className={item.id === currentId ? 'active' : ''} />
               </div>
@@ -51,14 +51,32 @@ export function Nft ({ characters }) {
         <div className="nft-info">
           <img className="nft-info__img" src={arrow} alt="" />
           <div className="nft-info__content">
-            <h3>{ character.name }</h3>
-            <p>{ t('nft.paragraph' ) }</p>
+            <h3>{character.name}</h3>
+            <p>{t('nft.paragraph')}</p>
             <div className="nft-info__properties">
               {character && (
                 <>
-                  <StatBar width={(character.damage / 100) * 100} value={`${character.damage} ед`} icon={damage} colors={['#FF7801', '#FF9501']} label={t('nft.performance.0')} />
-                  <StatBar width={(character.recharge / 8) * 100} value={`+${character.recharge}%/мин`} icon={recharge} colors={['#6C17FF', '#A50CFE']} label={t('nft.performance.1')} />
-                  <StatBar width={(parseFloat(character.hp) / 100.000) * 100} value={character.hp} icon={hp} colors={['#DD1440', '#FF2835']} label={t('nft.performance.2')} />
+                  <StatBar
+                    width={(character.damage / 100) * 100}
+                    value={`${character.damage} ${t('nft.performance.0.measure')}`}
+                    icon={damage}
+                    colors={['#FF7801', '#FF9501']}
+                    label={t('nft.performance.0.type')}
+                  />
+                  <StatBar
+                    width={(character.recharge / 8) * 100}
+                    value={`+${character.recharge}%/${t('nft.performance.1.measure')}`}
+                    icon={recharge}
+                    colors={['#6C17FF', '#A50CFE']}
+                    label={t('nft.performance.1.type')}
+                  />
+                  <StatBar
+                    width={(parseFloat(character.hp) / 100.0) * 100}
+                    value={character.hp}
+                    icon={hp}
+                    colors={['#DD1440', '#FF2835']}
+                    label={t('nft.performance.2.type')}
+                  />
                 </>
               )}
             </div>
